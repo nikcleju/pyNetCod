339a686b680cf252be9a63b5f6289e5cad3a9d0d
Nic <nikcleju@gmail.com>
2012-02-02 13:10:16 +0000

    Fixed HUGE bug in Matlab code: in compute_p0_matrix_single_r(), I wrongly used "if child == ncnodes" which returned a truth array, instead of using "if any(ncnodes == child)"
    Only minor changes in tests.py

--- tests.py
+++ tests.py
@@ -3,6 +3,8 @@ import numpy
 
 import examples
 
+#import unittest
+
 def testCreate():
     n_helpers = 25
     minnnodes = 20
@@ -17,6 +19,12 @@ def testLoadRun():
     examples.load_example_and_run('..\\ExNC\\2', {'do_global_flow':0,
                                                   'do_dist_delay':0,
                                                   'do_dist_flow':0})
+
+#class testComputings(unittest.TestCase):
+#    
+#def setUp(self):
+#    self.
+
 
 if __name__ == "__main__":
     testLoadRun()
